19b93f9bd63de8670a34b99aa86f1484c19b8f06
Paulo Evangelista <paulo.evangelista@sou.inteli.edu.br>
2023-03-17 11:35:13 -0300

    fix(backend): changed routes

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from controller import Controller
 from serial.tools import list_ports
+import pymysql
 
 
 # dobot = Controller()
@@ -11,29 +12,58 @@ dobotPort = "COM7"
 print("--------------------------------------------------")
 print("Servidor iniciando...")
 print("--------------------------------------------------")
-available_ports = list_ports.comports()
-print(f'Portas de comunicação disponíveis: {[x.device for x in available_ports]}')
+print("Testando conexão com banco de dados...")
 print("--------------------------------------------------")
-print(f"Conectando ao Dobot Magician Lite na porta {dobotPort}...")
-print("--------------------------------------------------")
-
 try:
-    dobot = Controller(dobotPort)
-except:
-    print("ERRO -> Não foi possível se conectar ao Dobot na porta específicada.")
-    # exit()
+    connection = pymysql.connect(
+        host='127.0.0.1',
+        port=3002,
+        user='user',
+        password='password',
+        database='db',
+        )
+    aa = connection.open    
+    print(aa)
+except Exception as e:
+    print("ERRO -> Não foi possível se conectar ao banco de dados.\n",e)
+    exit()
 
-print("--------------------------------------------------")
-print("Dobot conectado! Iniciando servidor")
 
-# rota que recebe as coordenadas desejadas e movimenta o robô
-@app.post("/movement")
-def coordinates():
-    (x, y, z, r, j1, j2, j3, j4) = request.form
-    return 200
+# with connection.cursor() as cursor:
+#     sql = "SELECT * FROM positions"
+#     cursor.execute(sql)
+#     result = cursor.fetchall()
+#     print(result)
 
-# rota que retorna as coordenadas da posição atual do robo
-@app.post("/position")
+# available_ports = list_ports.comports()
+# print(f'Portas de comunicação disponíveis: {[x.device for x in available_ports]}')
+# print("--------------------------------------------------")
+# print(f"Conectando ao Dobot Magician Lite na porta {dobotPort}...")
+# print("--------------------------------------------------")
+
+# try:
+#     dobot = Controller(dobotPort)
+# except:
+#     print("ERRO -> Não foi possível se conectar ao Dobot na porta específicada.")
+#     # exit()
+
+# print("--------------------------------------------------")
+# print("Dobot conectado! Iniciando servidor")
+
+# INSERE A POSIÇÃO ATUAL DO ROBO NO BANCO DE DADOS
+@app.get("/insertCurrentPosition")
+def insert():
+    try:
+        with connection.cursor() as cursor:
+            sql = "INSERT INTO positions (x, y, z, r, j1, j2, j3, j4) VALUES (20,20,20,20,20,20,20,20)"
+            cursor.execute(sql); cursor.fetchall()
+        return "OK", 200
+    
+    except Exception as e:
+        return e, 500
+
+# RETORNA A POSICAO ATUAL DO ROBO
+@app.get("/position")
 def position():
     try:
         (x, y, z, r, j1, j2, j3, j4) = dobot.pose()
@@ -41,3 +71,22 @@ def position():
 
     except Exception as e:
         return e, 500
+    
+# RETORNA AS 3 ULTIMAS POSIÇÕES GUARDADAS NO BANCO DE DADOS
+@app.get("/getLastPositions")
+def lastPositions():
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM positions ORDER BY id DESC LIMIT 3"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            print(result)
+            return jsonify(result), 200
+        
+    except Exception as e:
+        return e, 500
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000, debug=True)
