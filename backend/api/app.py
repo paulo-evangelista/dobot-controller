48e0db945209207162588efd08345a99b2e8d27e
Paulo <paulo.evangelista@sou.inteli.edu.br>
2023-03-28 02:47:11 -0300

    feat():removed mysql; added sqlite + ORM

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,34 +1,20 @@
-from flask import Flask, request, jsonify
-from backend.api.controller import Controller
+from flask import Flask, request, jsonify, make_response
+from controller import Controller
 from serial.tools import list_ports
-import pymysql
+from prisma import Prisma, register
+from prisma.models import Positions
+import jsonpickle
+
+db = Prisma()
+db.connect()
+register(db)
 
 
 # dobot = Controller()
 
 app = Flask(__name__)
 dobotPort = "COM7"
-
-print("--------------------------------------------------")
-print("Servidor iniciando...")
-print("--------------------------------------------------")
-print("Testando conexão com banco de dados...")
-print("--------------------------------------------------")
-try:
-    connection = pymysql.connect(
-        host='127.0.0.1',
-        port=3002,
-        user='user',
-        password='password',
-        database='db',
-        )
-    aa = connection.open    
-    print(aa)
-except Exception as e:
-    print("ERRO -> Não foi possível se conectar ao banco de dados.\n",e)
-    exit()
-
-
+app.config['JSON_AS_ASCII'] = False
 # with connection.cursor() as cursor:
 #     sql = "SELECT * FROM positions"
 #     cursor.execute(sql)
@@ -51,42 +37,27 @@ except Exception as e:
 # print("Dobot conectado! Iniciando servidor")
 
 # INSERE A POSIÇÃO ATUAL DO ROBO NO BANCO DE DADOS
-@app.get("/insertCurrentPosition")
-def insert():
-    try:
-        with connection.cursor() as cursor:
-            sql = "INSERT INTO positions (x, y, z, r, j1, j2, j3, j4) VALUES (20,20,20,20,20,20,20,20)"
-            cursor.execute(sql); cursor.fetchall()
-        return "OK", 200
-    
-    except Exception as e:
-        return e, 500
-
-# RETORNA A POSICAO ATUAL DO ROBO
-@app.get("/position")
-def position():
-    try:
-        (x, y, z, r, j1, j2, j3, j4) = dobot.pose()
-        return {x,y,z,j1,j2,j3,j4}, 200
-
-    except Exception as e:
-        return e, 500
-    
-# RETORNA AS 3 ULTIMAS POSIÇÕES GUARDADAS NO BANCO DE DADOS
 @app.get("/getLastPositions")
-def lastPositions():
-    try:
-        with connection.cursor() as cursor:
-            sql = "SELECT * FROM positions ORDER BY id DESC LIMIT 3"
-            cursor.execute(sql)
-            result = cursor.fetchall()
-            print(result)
-            return jsonify(result), 200
-        
-    except Exception as e:
-        return e, 500
+def teste():
+    Positions.prisma().create(data={"j1": 45,"j2":45,"j3":45})
+    tee = Positions.prisma().find_many(take=3, order={"id": "desc"})
+    data = []
+    for i in tee: data.append(i.__dict__)
+    resp = make_response("okok")
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
+    # return "okok", 200
 
+@app.post("/move")
+def move():
 
+    id = int(request.json["id"])
+
+    if not id:
+        return "nothing provided", 400
+    
+    data = Positions.prisma().find_unique(where={"id": id})
+    return jsonify(data.__dict__), 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000, debug=True)
--- godot/control.gd
+++ godot/control.gd
@@ -1,18 +1,18 @@
 extends Control
 
-var json = JSON.new()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 	
 func _on_atualizar_pressed():
-	$HTTPRequest.request("http://www.mocky.io/v2/5185415ba171ea3a00704eed")
+	$HTTPRequest.request("http://127.0.0.1:3000/getLastPositions")
 
 
-func _on_http_request_request_completed(result, response_code, headers, body):
-	var js = json.parse(body.get_string_from_utf8())
-	print(js)
-	
+func _on_HTTPRequest_request_completed( result, response_code, headers, body ):
+	#var json2 = JSON.new()
+	#var jsone = json2.parse(body.get_string_from_utf8())
+	print(headers)
+	print(body.size())
 	
 		#junta1
 	$MarginContainer/HBoxContainer/PanelContainer/VBoxContainer3/Junta1container/ProgressBar.value=response_code / 6
@@ -35,3 +35,5 @@ func _on_http_request_request_completed(result, response_code, headers, body):
 		#penultima junta3
 	$MarginContainer/HBoxContainer/PanelContainer2/VBoxContainer/ProgressBar6.value=response_code / 6
 	
+
+
