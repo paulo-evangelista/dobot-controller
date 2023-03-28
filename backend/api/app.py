4167dd9327b6144a7b81719c0a9d08efbbef26ee
Paulo Evangelista <paulo.evangelista@sou.inteli.edu.br>
2023-03-28 16:58:19 -0300

    feat(): integration completed

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,48 +1,103 @@
 from flask import Flask, request, jsonify, make_response
-from controller import Controller
 from serial.tools import list_ports
+import PySimpleGUI as sg
 from prisma import Prisma, register
 from prisma.models import Positions
 import pydobot
+import json
 
 db = Prisma()
 db.connect()
 register(db)
 app = Flask(__name__)
 
-dobotPort = "COM7"
-available_ports = list_ports.comports()
-print(f'Portas de comunicação disponíveis: {[x.device for x in available_ports]}')
-print("--------------------------------------------------")
-print(f"Conectando ao Dobot Magician Lite na porta {dobotPort}...")
-print("--------------------------------------------------")
-dobot = pydobot.Dobot(port=dobotPort, verbose=True)
 
 
+# available_ports = list_ports.comports()
+# print(f'Portas de comunicação disponíveis: {[x.device for x in available_ports]}')
+dobotPort = "COM3"
+
+
+
+# sg.theme('SystemDefault')
+# layout = [  [sg.Text('Selecione a porta de comunicação do Dobot:')],
+#             [sg.Text('Portas disponiveis:'), sg.Combo(values=[x.device for x in available_ports])],
+#             [sg.Button('Ok')]]
+
+# # Create the Window
+# window = sg.Window('Bem vindo', layout)
+# # Event Loop to process "events" and get the "values" of the inputs
+# while True:
+#     event, values = window.read()
+#     print('You entered ', values[0])
+#     if event == sg.WIN_CLOSED or event == 'Ok': # if user closes window or clicks cancel
+#         dobotPort = values[0]
+#         break
+
+# window.close()
+
+try:
+    dobot = pydobot.Dobot(port=dobotPort, verbose=True)
+except:
+    sg.popup('Erro ao conectar com o Dobot. Verifique se a porta selecionada está correta e se o Dobot está ligado.', title='Erro')
+    exit()
+# grava a posição atual do dobot no banco de dados e retorna os 3 últimos registros
 @app.get("/getLastPositions")
 def teste():
 
-    positions = dobot.pose()
+    # pega as posições atuais do dobot
+    dobot_positions = dobot.pose()
 
-    Positions.prisma().create(data={"j1": positions["j1"],"j2":positions["j2"],"j3":["j3"]})
+    #insere j1, j2 e j3 no banco de dados
+    Positions.prisma().create(data={"j1":dobot_positions[4],"j2":dobot_positions[5],"j3":dobot_positions[6]})
+    print("inserted ",dobot_positions[4],dobot_positions[5],dobot_positions[6], "  to DB.")
+
+    # pega os 3 registros mais recentes do banco de dados
     query_data = Positions.prisma().find_many(take=3, order={"id": "desc"})
-    result = []
 
+    # Os regitros são retornados como um arr de objetos, então é necessário transformar
+    # em um arr de dicionários para então podermos serializar um json
+    result = []
     for i in query_data: result.append(i.__dict__)
 
+    # retorna o json
     return jsonify(result), 200
 
+#rota que recebe um id, busca no banco de dados e move o dobot para a posição correspondente
 @app.post("/move")
-
 def move():
 
-    id = int(request.json["id"])
+    # pega o json recebido e transforma em dicionário
+    received = json.loads(request.data.decode("utf-8"))
 
-    if not id:
-        return "nothing provided", 400
+    # confere se o json recebido não está vazio
+    if not received: return "nothing provided", 400
+
+    # pega separa o id do dicionario
+    id = received["id"]
+
+    # confere se o id não é vazio
+    if not id: return "nothing provided", 400
     
+    # busca no banco de dados a posição correspondente ao id recebido
     data = Positions.prisma().find_unique(where={"id": id})
-    return jsonify(data.__dict__), 200
+
+    # Transforma o objeto retornado pelo db em um dicionário
+    positions_dict = data.__dict__
+    print(positions_dict)
+    print("OI", pydobot.enums.PTPMode.MOVJ_XYZ.value)
+    # move o dobot para a posição correspondente
+    dobot.pose()
+    dobot._set_ptp_cmd(
+        x=positions_dict["j1"],
+        y=positions_dict["j2"],
+        z=positions_dict["j3"],
+        r=4,
+        wait=True,
+        mode=pydobot.enums.PTPMode.MOVJ_ANGLE
+        )
+    # retorna status de success
+    return "ok", 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    app.run(host="0.0.0.0", port=3000, debug=True,use_reloader=False)
--- godot/control.gd
+++ godot/control.gd
@@ -1,23 +1,30 @@
 extends Control
 
+var ultimoID = 0
+var penultimoID = 0
+var loading_screen = preload("res://loading.tscn")
+var instance = loading_screen.instantiate()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
 func _on_atualizar_pressed():
+	add_child(instance)
 	var http_request = HTTPRequest.new()
 	add_child(http_request)
-	http_request.request_completed.connect(self._http_request_completed)
+	http_request.request_completed.connect(self._get_request_completed)
 	var error = http_request.request("http://127.0.0.1:3000/getLastPositions")
 	if error != OK:
 		push_error("An error occurred in the HTTP request.")
 
-# Called when the HTTP request is completed.
-func _http_request_completed(result, response_code, headers, body):
+func _get_request_completed(result, response_code, headers, body):
+	instance.queue_free()
+	instance = loading_screen.instantiate()
 	var json = JSON.new()
 	json.parse(body.get_string_from_utf8())
 	var response = json.get_data()
 	print(response)
+
 
 		#junta1
 	$MarginContainer/HBoxContainer/PanelContainer/VBoxContainer3/Junta1container/ProgressBar.value=response[0]["j1"]
@@ -26,12 +33,13 @@ func _http_request_completed(result, response_code, headers, body):
 		#junta3
 	$MarginContainer/HBoxContainer/PanelContainer/VBoxContainer3/junta3container/ProgressBar.value=response[0]["j3"]
 
-		#ultima junta1#
+		#ultima junta1
 	$MarginContainer/HBoxContainer/PanelContainer2/VBoxContainer/ProgressBar1.value=response[1]["j1"]
 		#ultima junta2
 	$MarginContainer/HBoxContainer/PanelContainer2/VBoxContainer/ProgressBar2.value=response[1]["j2"]
 		#ultima junta3
 	$MarginContainer/HBoxContainer/PanelContainer2/VBoxContainer/ProgressBar3.value=response[1]["j3"]
+	ultimoID = response[1]["id"]
 	
 		#penultima junta1
 	$MarginContainer/HBoxContainer/PanelContainer2/VBoxContainer/ProgressBar4.value=response[2]["j1"]
@@ -39,6 +47,30 @@ func _http_request_completed(result, response_code, headers, body):
 	$MarginContainer/HBoxContainer/PanelContainer2/VBoxContainer/ProgressBar5.value=response[2]["j2"]
 		#penultima junta3
 	$MarginContainer/HBoxContainer/PanelContainer2/VBoxContainer/ProgressBar6.value=response[2]["j3"]
+	penultimoID = response[2]["id"]
 
 
+func makePost(id: int):
+	add_child(instance)
+	var link = "http://127.0.0.1:3000/move"
+	var http_request = HTTPRequest.new()
+	add_child(http_request)
+	http_request.request_completed.connect(self._http_request_completed)
+	var body = JSON.new().stringify({"id": id})
+	var error = http_request.request(link, [], HTTPClient.METHOD_POST, body)
+	if error != OK:
+		push_error("An error occurred in the HTTP request.")
 
+func _http_request_completed(result, response_code, headers, body):
+	instance.queue_free()
+	instance = loading_screen.instantiate()
+	
+	
+func _on_voltar_ultima_pressed():
+	print(ultimoID)
+	makePost(ultimoID)
+
+
+func _on_voltar_penultima_pressed():
+	print(penultimoID)
+	makePost(penultimoID)
