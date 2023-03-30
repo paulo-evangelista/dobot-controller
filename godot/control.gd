d2012cd7c3366f02d46663c9137edf017a563e0f
Paulo Evangelista <paulo.evangelista@sou.inteli.edu.br>
2023-03-30 11:36:55 -0300

    feat(): finished location system

--- godot/control.gd
+++ godot/control.gd
@@ -3,6 +3,8 @@ extends Control
 var ultimoID = 0
 var loading_screen = preload("res://loading.tscn")
 var instance = loading_screen.instantiate()
+var redDotLastX = 0
+var redDotLastY = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -20,38 +22,38 @@ func _get_request_completed(result, response_code, headers, body):
 	var json = JSON.new()
 	json.parse(body.get_string_from_utf8())
 	var response = json.get_data()
-	placeRedDot(response)
+	placeRedDot(response["coordinates"])
 	print(response)
 
 		#ultimo ID
-	ultimoID = response[0]["id"]
+	ultimoID = response["joints"][0]["id"]
 		#junta1
-	$MarginContainer/HBoxContainer/PanelContainer/VBoxContainer3/Junta1container/ProgressBar.value=response[0]["j1"]
+	$MarginContainer/HBoxContainer/PanelContainer/VBoxContainer3/Junta1container/ProgressBar.value=response["joints"][0]["j1"]
 		#junta2
-	$MarginContainer/HBoxContainer/PanelContainer/VBoxContainer3/Junta2container/ProgressBar.value=response[0]["j2"]
+	$MarginContainer/HBoxContainer/PanelContainer/VBoxContainer3/Junta2container/ProgressBar.value=response["joints"][0]["j2"]
 		#junta3
-	$MarginContainer/HBoxContainer/PanelContainer/VBoxContainer3/junta3container/ProgressBar.value=response[0]["j3"]
+	$MarginContainer/HBoxContainer/PanelContainer/VBoxContainer3/junta3container/ProgressBar.value=response["joints"][0]["j3"]
 
 		#ultima junta1
-	$MarginContainer/HBoxContainer/PanelContainer2/VBoxContainer/ProgressBar7.value=response[0]["j1"]
+	$MarginContainer/HBoxContainer/PanelContainer2/VBoxContainer/ProgressBar7.value=response["joints"][0]["j1"]
 		#ultima junta2
-	$MarginContainer/HBoxContainer/PanelContainer2/VBoxContainer/ProgressBar8.value=response[0]["j2"]
+	$MarginContainer/HBoxContainer/PanelContainer2/VBoxContainer/ProgressBar8.value=response["joints"][0]["j2"]
 		#ultima junta3
-	$MarginContainer/HBoxContainer/PanelContainer2/VBoxContainer/ProgressBar9.value=response[0]["j3"]
+	$MarginContainer/HBoxContainer/PanelContainer2/VBoxContainer/ProgressBar9.value=response["joints"][0]["j3"]
 	
 		#penultima junta1
-	$MarginContainer/HBoxContainer/PanelContainer2/VBoxContainer/ProgressBar1.value=response[1]["j1"]
+	$MarginContainer/HBoxContainer/PanelContainer2/VBoxContainer/ProgressBar1.value=response["joints"][1]["j1"]
 		#penultima junta2
-	$MarginContainer/HBoxContainer/PanelContainer2/VBoxContainer/ProgressBar2.value=response[1]["j2"]
+	$MarginContainer/HBoxContainer/PanelContainer2/VBoxContainer/ProgressBar2.value=response["joints"][1]["j2"]
 		#penultima junta3
-	$MarginContainer/HBoxContainer/PanelContainer2/VBoxContainer/ProgressBar3.value=response[1]["j3"]
+	$MarginContainer/HBoxContainer/PanelContainer2/VBoxContainer/ProgressBar3.value=response["joints"][1]["j3"]
 	
 		#antepenultima junta1
-	$MarginContainer/HBoxContainer/PanelContainer2/VBoxContainer/ProgressBar4.value=response[2]["j1"]
+	$MarginContainer/HBoxContainer/PanelContainer2/VBoxContainer/ProgressBar4.value=response["joints"][2]["j1"]
 		#antepenultima junta2
-	$MarginContainer/HBoxContainer/PanelContainer2/VBoxContainer/ProgressBar5.value=response[2]["j2"]
+	$MarginContainer/HBoxContainer/PanelContainer2/VBoxContainer/ProgressBar5.value=response["joints"][2]["j2"]
 		#antepenultima junta3
-	$MarginContainer/HBoxContainer/PanelContainer2/VBoxContainer/ProgressBar6.value=response[2]["j3"]
+	$MarginContainer/HBoxContainer/PanelContainer2/VBoxContainer/ProgressBar6.value=response["joints"][2]["j3"]
 
 	instance.queue_free()
 	instance = loading_screen.instantiate()
@@ -95,4 +97,10 @@ func _on_repetir_pressed():
 
 # função para o visualizador de posição da garra
 func placeRedDot(data):
-	pass
+	$Container/Ellipse2.move_local_y(-redDotLastY)
+	$Container/Ellipse2.move_local_x(-redDotLastX)
+	
+	$Container/Ellipse2.move_local_y(-data[0] / 1.7)
+	$Container/Ellipse2.move_local_x(-data[1]  /2)
+	redDotLastY = -data[0] / 1.7
+	redDotLastX = -data[1] / 2
